540539aa3c7fb897870d9a261b196b5e462f6e23
sideseal <gychoi@student.42seoul.kr>
2022-07-12 13:54:20 +0900

    modify: modify logic

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gychoi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 18:11:49 by gychoi            #+#    #+#             */
-/*   Updated: 2022/07/11 14:54:40 by gychoi           ###   ########.fr       */
+/*   Updated: 2022/07/12 13:33:33 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if ((s1[i] < s2[i]) || (s1[i] > s2[i]))
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if ((((unsigned char *)s1)[i] < ((unsigned char *)s2)[i]) || \
+			(((unsigned char *)s1)[i] > ((unsigned char *)s2)[i]))
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
 	return (0);
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gychoi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 15:39:22 by gychoi            #+#    #+#             */
-/*   Updated: 2022/07/11 16:19:46 by gychoi           ###   ########.fr       */
+/*   Updated: 2022/07/12 13:51:54 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,24 @@ int	check_set(char const *set, char ch)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	strlen;
+	size_t	slen;
 	size_t	i;
 	size_t	j;
 	char	*string;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	strlen = ft_strlen(s1);
-	string = malloc(sizeof(char) * strlen + 1);
+	slen = ft_strlen(s1);
+	string = malloc(sizeof(char) * slen + 1);
 	if (!string)
 		return (NULL);
 	i = 0;
-	if (check_set(set, s1[0]))
+	while (check_set(set, s1[i]))
 		i++;
-	if (check_set(set, s1[strlen - 1]))
-		strlen--;
+	while (check_set(set, s1[slen - 1]))
+		slen--;
 	j = 0;
-	while (i < strlen)
+	while (i < slen)
 		string[j++] = s1[i++];
 	string[j] = '\0';
 	return (string);
