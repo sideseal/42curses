2c07f7f382393a155c619e3a18553aed15583eb3
sideseal <gychoi@student.42seoul.kr>
2022-08-31 17:58:22 +0900

    added: add get_next_line function to libft

---
+++ libft/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 16:24:36 by gychoi            #+#    #+#             */
+/*   Updated: 2022/08/29 20:30:19 by gychoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*gnl_strdup(char *s1);
+char	*gnl_strjoin(char *s1, char *s2);
+
+typedef struct s_list
+{
+	int				fd;
+	char			*backup;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*gnl_lstset(t_list **head, int fd);
+t_list	*gnl_lstnew(int fd);
+void	*gnl_lstclear(t_list **node, int fd);
+
+#endif
