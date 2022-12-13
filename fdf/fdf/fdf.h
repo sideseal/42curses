d5b928c3990322d882e4b2012602fdd4bfa4020b
sideseal <gychoi@student.42seoul.kr>
2022-12-13 21:04:50 +0900

    20221213

--- fdf/fdf/fdf.h
+++ fdf/fdf/fdf.h
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 22:32:08 by gychoi            #+#    #+#             */
-/*   Updated: 2022/12/10 22:46:20 by gychoi           ###   ########.fr       */
+/*   Updated: 2022/12/13 21:02:27 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,38 @@
 # define FDF_H
 
 # include "mlx/mlx.h"
+# include "libft/libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
 
-typedef struct	s_var
+typedef struct	s_ctx
 {
 	void	*mlx;
 	void	*win;
-}	t_var;
+	char	**map;
+	int		width;
+	int		height;
+}	t_ctx;
 
 typedef struct	s_fdf
 {
 	void	*img;
 	char	*addr;
-	int	bpp;
-	int	line_len;
-	int	endian;
+	int		bpp;
+	int		line_len;
+	int		endian;
 }	t_fdf;
+
+void	fdf_error(void);
+
+t_ctx	*init_ctx(char *path, int img_width, int img_height);
+t_fdf	*init_fdf(t_ctx *ctx, int img_width, int img_height);
+
+char	**read_from_file(t_ctx *ctx, char *path);
+int	get_height(char **map);
+int	get_width(char *line);
 
 #endif
--- fdf/fdf/main.c
+++ fdf/fdf/main.c
@@ -6,58 +6,28 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 18:33:07 by gychoi            #+#    #+#             */
-/*   Updated: 2022/12/10 23:04:23 by gychoi           ###   ########.fr       */
+/*   Updated: 2022/12/13 21:03:17 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	my_mlx_pixel_put(t_fdf *data, int x, int y, int color)
+int	main(int argc, char **argv)
 {
-	char	*dst;
+	t_ctx	*ctx;
+	t_fdf	*fdf;
+	int		img_width;
+	int		img_height;
 
-	dst = data->addr + (y * data->line_len + x * (data->bpp / 8));
-	*(unsigned int *)dst = color;
-}
-
-int	key_hook(int keycode, t_var *var)
-{
-	if (keycode == 53)
-	{
-		mlx_destroy_window(var->mlx, var->win);
-		exit(0);
-	}
-	return (0);
-}
-
-int	exit_hook(void)
-{
-	exit(0);
-}
-
-int	main(void)
-{
-	t_var	var;
-	t_fdf	img;
-
-	var.mlx = mlx_init();
-	var.win = mlx_new_window(var.mlx, 1024, 1024, "Hello world!");
-	img.img = mlx_new_image(var.mlx, 1024, 1024);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-	for (int i = 0; i < 1024 - 1; i++)
-	{
-		for (int j = 0; j < 1024 - 1; j++)
-		{
-			double	r = (double)(1024 - j) / (1024 - 1);
-			double	g = (double)(i) / (1024 - 1);
-			double	b = 1;
-			int	color = ((int)(255.999 * r) << 16) + ((int)(255.999 * g) << 8) + ((int)(255.999 * b));
-			my_mlx_pixel_put(&img, j, i, color);
-		}
-	}
-	mlx_put_image_to_window(var.mlx, var.win, img.img, 0, 0);
-	mlx_key_hook(var.win, key_hook, &var);
-	mlx_hook(var.win, 17, 0, exit_hook, 0);
-	mlx_loop(var.mlx);
+	// argc 3개 이상인 경우도 처리할 것.
+	if (argc < 2)
+		return (0);
+	img_width = 720;
+	img_height = 720;
+	ctx = init_ctx(argv[1], img_width, img_height);
+	fdf = init_fdf(ctx, img_width, img_height);
+	printf("width: %d, height: %d\n", ctx->width, ctx->height);
+	for (int i = 0; i < 9; i++)
+		printf("line : %s\n", ctx->map[i]);
 	return (0);
 }
