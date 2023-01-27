e0b35d48ecb2dd6a006ba4bec5c62bc986130950
sideseal <gychoi@student.42seoul.kr>
2023-01-27 21:24:53 +0900

    modify: norm error in header file

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gychoi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 12:30:05 by gychoi            #+#    #+#             */
-/*   Updated: 2022/12/13 15:43:32 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/01/27 19:24:57 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,6 @@ t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 
 char	*get_next_line(int fd);
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 
 #endif
