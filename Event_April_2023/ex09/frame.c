ba78d9248d60db42624ea4eda96067e364de34f4
sideseal <gychoi@student.42seoul.kr>
2023-04-01 23:18:29 +0900

    wasting my blackhole

--- Event_April_2023/ex09/frame.c
+++ Event_April_2023/ex09/frame.c
@@ -31,28 +31,34 @@ int	get_max_strlen(char *string, char *delim)
 int	main(int argc, char **argv)
 {
 	int		max;
+	int		index;
 	int		i;
 	char	*token;
 	char	delim[] = " \n\t\v\f\r";
 
 	if (argc == 1)
 		return (1);
-	max = get_max_strlen(argv[1], delim);
-	i = -1;
-	while (++i < max + 4)
-		printf("*");
-	printf("\n");
-	token = strtok(argv[1], delim);
-	while (token != NULL)
+	index = 1;
+	while (argv[index] != 0)
 	{
-		printf("* ");
-		printf("%-*s", max, token);
-		printf(" *\n");
-		token = strtok(NULL, delim);
+		max = get_max_strlen(argv[index], delim);
+		i = -1;
+		while (++i < max + 4)
+			printf("*");
+		printf("\n");
+		token = strtok(argv[index], delim);
+		while (token != NULL)
+		{
+			printf("* ");
+			printf("%-*s", max, token);
+			printf(" *\n");
+			token = strtok(NULL, delim);
+		}
+		i = -1;
+		while (++i < max + 4)
+			printf("*");
+		printf("\n");
+		index++;
 	}
-	i = -1;
-	while (++i < max + 4)
-		printf("*");
-	printf("\n");
 	return (0);
 }
