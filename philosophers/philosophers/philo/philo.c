650b151cb6aeec0c7f6cd4b23bd6211122f081aa
sideseal <gychoi@student.42seoul.kr>
2023-04-06 23:05:44 +0900

    20230406

--- philosophers/philosophers/philo/philo.c
+++ philosophers/philosophers/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:11:15 by gychoi            #+#    #+#             */
-/*   Updated: 2023/03/31 23:23:24 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/04/06 23:04:39 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,28 @@ void	*test(void *data)
 	t_philo	*philo;
 
 	philo = (t_philo *)data;
-	printf("%d, [%d, %d, %d, %d]\n", philo->philo_name, philo->resources.forks[0], philo->resources.forks[1], philo->resources.forks[2], philo->resources.forks[3]);
+	while (1)
+	{
+		pthread_mutex_lock(philo->fork_mutex[0]);
+		*(philo->fork[0]) = 1;
+		pthread_mutex_unlock(philo->fork_mutex[0]);
+		pthread_mutex_lock(philo->fork_mutex[1]);
+		*(philo->fork[1]) = 1;
+		pthread_mutex_unlock(philo->fork_mutex[1]);
+
+		usleep(100000);
+
+		pthread_mutex_lock(philo->fork_mutex[0]);
+		*(philo->fork[0]) = 0;
+		pthread_mutex_unlock(philo->fork_mutex[0]);
+		pthread_mutex_lock(philo->fork_mutex[1]);
+		*(philo->fork[1]) = 0;
+		pthread_mutex_unlock(philo->fork_mutex[1]);
+
+		pthread_mutex_lock(&(philo->resources.print_mutex));
+		//printf("%d, [%d, %d, %d, %d]\n", philo->philo_name, philo->resources.forks[0], philo->resources.forks[1], philo->resources.forks[2], philo->resources.forks[3]);
+		pthread_mutex_unlock(&(philo->resources.print_mutex));
+	}
 	return ((void *)0);
 }
 
@@ -43,21 +64,32 @@ void	start_simulation(t_context context, t_philo *philos)
 	}
 }
 
+void	check_leaks(void)
+{
+	system("leaks philo");
+}
+
 int	main(int argc, char **argv)
 {
 	t_context		context;
 	t_philo			*philos;
 	t_resource		resources;
 
+	//atexit(check_leaks);
 	if (argc < 5 || argc > 6 || !check_valid_input(argv))
 	{
 		printf("Usage: ./philo number_of_philosophers time_to_die time_to_eat"
 			" time_to_sleep [number_of_times_each_philosopher_must_eat]");
 		return (1);
 	}
-	init_context(&context, argv);
-	init_resources(&resources, context);
-	init_philos(&philos, context, resources);
+	if (init_context(&context, argv) < 0)
+		return (1);
+	if (init_resources(&resources, context) < 0)
+		return (1);
+	if (init_philos(&philos, context, resources) < 0)
+		return (1);
 	start_simulation(context, philos);
+	ft_free_both(resources.forks, resources.forks_mutex);
+	free(philos);
 	return (0);
 }
--- philosophers/philosophers/philo/philo.h
+++ philosophers/philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:23:29 by gychoi            #+#    #+#             */
-/*   Updated: 2023/03/31 23:13:02 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/04/06 22:24:49 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,30 @@ typedef struct s_context
 typedef struct s_resource
 {
 	int				*forks;
-	pthread_mutex_t	mutex_print;
-	pthread_mutex_t	mutex_forks;
+	pthread_mutex_t	*forks_mutex;
+	pthread_mutex_t	print_mutex;
 }	t_resource;
 
 typedef struct s_philo
 {
-	pthread_t	philo_thread;
-	t_resource	resources;
-	int			philo_name;
-	int			*fork[2];
-	int			time_die;
-	int			time_eat;
-	int			time_sleep;
-	int			count_eat;
-	int			philo_must_eat;
-	int			philo_is_dead;
+	pthread_t		philo_thread;
+	t_resource		resources;
+	int				philo_name;
+	int				*fork[2];
+	pthread_mutex_t	*fork_mutex[2];
+	t_context		context;
+	int				count_eat;
+	int				philo_is_dead;
 }	t_philo;
 
 int			check_valid_input(char **argv);
 int			philo_atoi(char *num);
 long long	get_millisecond(void);
-void		init_context(t_context *context, char **argv);
-void		init_philos(t_philo **philos, t_context context, \
+int			ft_free_both(void *ptr1, void *ptr2);
+int			init_context(t_context *context, char **argv);
+int			init_philos(t_philo **philos, t_context context, \
 			t_resource resources);
-void		init_resources(t_resource *resources, t_context context);
+int			init_resources(t_resource *resources, t_context context);
 void		start_simulation(t_context context, t_philo *philos);
 
 #endif
