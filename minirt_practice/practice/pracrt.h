477582eb20cc249525ae54ada14bd635aba00065
sideseal <gychoi@student.42seoul.kr>
2023-05-09 23:05:52 +0900

    20230509

---
+++ minirt_practice/practice/pracrt.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pracrt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 17:05:13 by gychoi            #+#    #+#             */
+/*   Updated: 2023/05/09 17:06:02 by gychoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRACRT_H
+# define PRACRT_H
+
+# include <mlx.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define SCREEN_WIDTH 400
+# define SCREEN_HEIGHT 300
+# define TRUE 1
+# define FALSE 0
+# define EPSILON 1e-6
+
+#endif
