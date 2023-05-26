c239cc786ca5174d9feb7640e76ab6381669a3f4
sideseal <gychoi@student.42seoul.kr>
2023-05-26 22:02:20 +0900

    20230526

--- .misc/practice/hit.c
+++ .misc/practice/hit.c
@@ -6,27 +6,44 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 20:03:37 by gychoi            #+#    #+#             */
-/*   Updated: 2023/05/25 22:26:21 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/05/26 22:01:29 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hit.h"
 
-t_bool	hit_sphere(t_sphere sp, t_ray ray, t_hit_record rec)
+void	set_face_normal(t_ray r, t_hit_record *rec)
+{
+	rec->front_face = 
+
+t_bool	hit_sphere(t_sphere sp, t_ray ray, t_hit_record *rec)
 {
 	t_vec3	cp;
 	double	a;
-	double	b;
+	double	half_b;
 	double	c;
 	double	disc;
+	double	sqrtd;
 	double	root;
 
 	cp = vsub(ray.origin, sp.center);
-	a = vdot(ray.dir, ray.dir);
-	b = 2.0 * vdot(cp, ray.dir);
-	c = vdot(cp, cp) - (sp.radius * sp.radius);
-	disc = b * b - 4 * a * c;
+	a = vlen_pow(ray.dir);
+	half_b = vdot(cp, ray.dir);
+	c = vlen_pow(cp) - (sp.radius * sp.radius);
+	disc = b * b - a * c;
 	if (disc < 0)
 		return (FALSE);
-	// ...
+	sqrtd = sqrt(disc);
+	root = (-half_b - sqrtd) / a;
+	if (root < rec->tmin || rec->tmax < root)
+	{
+		root = (-half_b + sqrtd) / a;
+		if (root < rec->tmin || rec->tmax < root)
+			return (FALSE);
+	}
+	rec->t = root;
+	rec->p = ray_at(ray, root);
+	rec->normal = vdivs(vsub(rec->p, sp.center), sp.radius);
+	set_face_normal(ray, rec);
+	return (TRUE);
 }
