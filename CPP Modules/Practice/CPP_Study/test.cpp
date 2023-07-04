6334dd3c6fe085541c92b4131889bf6541a537d0
sideseal <gychoi@student.42seoul.kr>
2023-07-04 23:32:31 +0900

    20230704

--- CPP Modules/Practice/CPP_Study/test.cpp
+++ CPP Modules/Practice/CPP_Study/test.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
+
+float add(float a, float b)
+{
+	float result = a + b;
+	return (result);
+}
+
+int add(int a)
+{
+	int result = a + 1;
+	return (result);
+}
+
 int	main()
 {
-	int hp = 100;
-	bool isIt = true;
-	cout << hp << endl;
-	cout << isIt + 1 << endl;
+	int	a = 3;
+	int	b = 5;
+	float c = 1.5f;
+	float d = 2.1f;
+	int result1 = add(a, b);
+	float result2 = add(c, d);
+	int result3 = add(a);
+
+	cout << result1 << ", " << result2 << ", " << result3 << endl;
+	return (0);
 }
