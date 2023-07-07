0573c93d4d2ed68920cad79d2bffed187a94da76
sideseal <gychoi@student.42seoul.kr>
2023-07-07 22:52:46 +0900

    20230707

--- CPP Modules/Practice/CPP_Study/test.cpp
+++ CPP Modules/Practice/CPP_Study/test.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void	many(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k)
-{
-	int result = a + b + c + d + e + f + g + h + i + j + k;
-	cout << result << endl;
-}
-
-void	setHp(int* hp)
-{
-	*hp = 100;
-	return ;
-}
-
 int	main()
 {
-	int	hp = 3;
-
-	many(1, 2, hp, 4, 5, 6, 7, 8, 9, 10, 11);
-	setHp(&hp);
+	int array[2][3] = { {1, 2, 3}, {4, 5, 6}};
+	cout << array << endl;
 	return (0);
 }
