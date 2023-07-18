66219b6a81d71da37f17e2f7970d8412e66d6c8a
sideseal <gychoi@student.42seoul.kr>
2023-07-18 23:18:50 +0900

    20230718

--- CPP Modules/CPP Module 00/ex00/megaphone.cpp
+++ CPP Modules/CPP Module 00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 21:06:13 by gychoi            #+#    #+#             */
-/*   Updated: 2023/07/17 22:13:54 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/07/18 23:17:42 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,15 @@ int	main(int argc, char* argv[])
 {
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		std::string s("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+		std::cout << s;
 	}
-	for (size_t i = 1; argv[i] != 0; i++)
+	else
 	{
-		for (size_t j = 0; j < std::char_traits<char>::length(argv[i]); j++)
-			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		for (size_t i = 1; argv[i] != 0; i++)
+			for (size_t j = 0; j < std::char_traits<char>::length(argv[i]); j++)
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
 	}
-	std::cout << std::endl;
+	std::cout << '\n';
 	return (0);
 }
