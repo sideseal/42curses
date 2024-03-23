ae1e42091d65dfe639c93c40a4cbf47aebf447ba
sideseal <ckgun@naver.com>
2024-03-23 23:55:22 +0900

    20240323

--- inception/inception/srcs/requirements/mariadb/tools/healthcheck.sh
+++ inception/inception/srcs/requirements/mariadb/tools/healthcheck.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
+# 그냥 밖으로 빼도 괜찮을 것 같은데 일단 나중에...
 mariadb-admin ping -h localhost -uroot -p${MYSQL_ROOT_PASSWORD} || exit 1
--- inception/inception/srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ inception/inception/srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -ex
 
 sed -i \
     -e 's|access_log /var/log/nginx/access.log main;|access_log /dev/stdout main;|g' \
-    -e 's|error_log /var/log/nginx/error.log warn;|error_log /dev/stderr notice;|g' \
+    -e 's|error_log /var/log/nginx/error.log warn;|error_log /dev/stderr warn;|g' \
     -e 's|ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;|ssl_protocols TLSv1.2 TLSv1.3;|g' \
     /etc/nginx/nginx.conf
 
