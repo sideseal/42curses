54c25b304a2d8e6f6da037521e0c881681741098
sideseal <ckgun@naver.com>
2024-03-23 23:43:18 +0900

    20240323

--- inception/inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ inception/inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -7,11 +7,8 @@ MYSQL_DATABASE=${MYSQL_DATABASE:-""}
 MYSQL_USER=${MYSQL_USER:-""}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-
 if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-    echo >&2 'error: MYSQL_ROOT_PASSWORD not set'
+    echo >&2 'error: MYSQL_ROOT_PASSWORD is not set'
     return 1
 fi
 
@@ -21,26 +18,43 @@ if [ ! -f $tmpfile ]; then
     return 1
 fi
 
-cat << EOF > $tmpfile
+createDatabase=
+createUser=
+grantPrivileges=
+
+if [ "$MYSQL_DATABASE" != "" ]; then
+    createDatabase="CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+fi
+
+if [ "$MYSQL_USER" != "" ]; then
+    if [ "$MYSQL_PASSWORD" = "" ]; then
+        echo >&2 'error: MYSQL_USER is specified but MYSQL_PASSWORD is not set'
+        return 1
+    else
+        createUser="CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+        ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    fi
+fi
+
+if [ "$MYSQL_DATABASE" != "" ] && [ "$MYSQL_USER" != "" ]; then
+    grantPrivileges="GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+fi
+
+cat <<-EOF > $tmpfile  
     USE mysql;
     FLUSH PRIVILEGES;
     ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
     GRANT ALL ON *.* TO root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
     DROP DATABASE IF EXISTS test;
-    
-    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-    
-    CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-    ALTER USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-    GRANT ALL ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;
+    $createDatabase
+    $createUser
+    $grantPrivileges
     FLUSH PRIVILEGES;
 EOF
 
-# 나눠야 한다...
-
-mariadbd --user=mysql --datadir=/var/lib/mysql --bootstrap < $tmpfile
+/usr/bin/mariadbd --user=mysql --bootstrap < $tmpfile
 rm -f $tmpfile
 
 echo 'Starting MariaDB Container...'
 
-exec mariadbd --user=mysql --datadir=/var/lib/mysql $@
+exec /usr/bin/mariadbd --user=mysql --console $@
